5c3749ff59f898bcef65889360f308096d8ae5c8
JaeSunEo <wotjs020708@gmail.com>
2025-03-17 19:36:05 +0900

    v0.3 O(log n) Guess game

--- week02.py
+++ week02.py
@@ -1,4 +1,17 @@
+import random
 
-n = int(input("정수 입력 : "))
-result = n * (n + 1) // 2 # O(1)
-print(result)
+r = random.randint(1, 100)
+
+c = 7
+
+for i in range(c):
+    n = int(input("정수 입력 : "))
+    if r > n:
+        print("정답보다 숫자가 큽니다.")
+    elif  r < n:
+        print("정답보다 숫자가 작습니다.")
+    else:
+        print("정답입니다.")
+        break
+    c -= 1
+    print("남은 횟수는:", c)

0066af16a09f0fcef526600c333043b33737f269
JaeSunEo <wotjs020708@gmail.com>
2025-03-24 18:12:46 +0900

    v1.1 array

--- week03.py
+++ week03.py
@@ -1,9 +1,10 @@
 def inters(l1, l2):
-    l3 = []
-    for v in l1:
-        if v in l2:
-            l3.append(v)
-    return  l3
+    s1 = set(l1)
+    s2 = set(l2)
+    # return  list(s1.intersection(s2)) # 교집합
+    return  list(s1 & s2)
+
+
 
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [22, 1, 5, 2, 7, 28, 27, 19, 13, 41]

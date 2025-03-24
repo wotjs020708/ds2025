3c919bf9a45cc07e4b333fc468852f89f5b40d27
JaeSunEo <wotjs020708@gmail.com>
2025-03-24 17:38:53 +0900

    v0.7

--- week03.py
+++ week03.py
@@ -1,13 +1,5 @@
-def move_zeros(l):
-    zero_idx = 0
-    for i in range(len(l)):
-        n = l[i]
-        if n != 0:
-            l[zero_idx] = n
-            if zero_idx != i:
-                l[i] = 0
-            zero_idx = zero_idx + 1
-    return l
+groups = ['HOT', 'Seventeen', 'Black Pink', "NJZ"]
+ratings = [1, 2, 4, 3]
 
-l = [99, 0, -7, 0, 16]
-print(move_zeros(l))
+group_rating = list(zip(groups, ratings))
+print(group_rating)

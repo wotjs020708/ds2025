871b74ee9fc1daf20f390249d71cb9f0809bd60b
JaeSunEo <wotjs020708@gmail.com>
2025-03-24 17:58:16 +0900

    v0.9 array

--- week03.py
+++ week03.py
@@ -1,5 +1,24 @@
+def duplicate_city(cities):
+    result_city = []
+    s = set()
+
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+
+        if l1 == l2:
+            result_city.append(city)
+
+    return result_city
+
+
 cities = ['Incheon', 'Seoul', 'Incheon','Incheon', 'Gwangju']
-cities = set(cities)
-cities.add('Incheon')
-cities.add('Suwon')
+# cities = set(cities)
+# cities.add('Incheon')
+# cities.add('Suwon')
+cities.append('Incheon')
+cities.append('Suwon')
+cities.append('Seoul')
 print(cities)
+print(set(duplicate_city(cities)))

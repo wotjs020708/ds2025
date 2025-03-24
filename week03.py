4820b0f2dfc90576a2e29dbd5bfdc2b07c659d80
JaeSunEo <wotjs020708@gmail.com>
2025-03-24 17:18:17 +0900

    v0.6

--- week03.py
+++ week03.py
@@ -1,7 +1,13 @@
-import array
+def move_zeros(l):
+    zero_idx = 0
+    for i in range(len(l)):
+        n = l[i]
+        if n != 0:
+            l[zero_idx] = n
+            if zero_idx != i:
+                l[i] = 0
+            zero_idx = zero_idx + 1
+    return l
 
-arr = array.array('i',[99, 0, -7, 0, 16])
-
-for i in range(len(arr)):
-    print(f"{arr[i]:3}  {id(arr[i])}")
-
+l = [99, 0, -7, 0, 16]
+print(move_zeros(l))

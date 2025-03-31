8b71f8462ddaf33b50738ec96c7304133aba4df2
JaeSunEo <wotjs020708@gmail.com>
2025-03-31 18:21:51 +0900

    v1.8 linked list

--- week04.py
+++ week04.py
@@ -21,14 +21,17 @@ class LinkedList:
 
 
     def remove(self, target):
+        current = self.head
         if self.head.data == target:
             self.head = self.head.link
+            current.link = None
             return
-        current = self.head
+
         previous = None
         while current:
             if target == current.data:
                 previous.link = current.link
+                current.link = None
             previous = current
             current = current.link
 
@@ -60,5 +63,6 @@ ll.append(-9)
 print(ll)
 print(ll.search(99))
 print(ll.search(10))
-ll.remove(8)
+ll.remove(10)
+ll.remove(77)
 print(ll)

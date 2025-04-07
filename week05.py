1b3c3e65446cd71b556d69c1a9aa51d76dca1f6e
JaeSunEo <wotjs020708@gmail.com>
2025-04-07 17:49:59 +0900

    v2.3 stack

--- week05.py
+++ week05.py
@@ -1,42 +1,11 @@
-class Node:
-    def __init__(self, data, link=None):
-        self.data = data
-        self.link = link
+s1 = []
 
-
-class Stack:
-    def __init__(self):
-        self.top = None
-
-    def push(self, data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node
-
-
-    def pop(self):
-        if self.top is None:
-            return "Stack is empty!"
-        popped_node = self.top
-        self.top = self.top.link
-        popped_node.link = None
-        return popped_node.data
-
-    def peek(self):
-        return self.top.data
-
-
-s1 = Stack()
-# print(s1.pop())
-s1.push("Data structure")
-s1.push("Database")
-# print(s1.pop())
-# print(s1.pop())
-s1.pop()
-print(s1.peek())
-
-# for i in range(2):
-#     print(s1.pop())
+s1.append("Data sturcture") #push
+s1.append("Database")
+print(s1[-1])  # peek
+print(s1.pop()) # pop
+print(s1)
+print(s1.pop()) # pop
+print(s1)
+print(s1.pop()) # IndexError: pop from empty list
+print(s1)

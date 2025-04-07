c05f46ced205e4419b9b061390f7fca61b06c2ed
JaeSunEo <wotjs020708@gmail.com>
2025-04-07 18:35:46 +0900

    v2.5 stack

--- week05.py
+++ week05.py
@@ -1,16 +1,17 @@
-# print(1+2))
+
 def is_valid_parentheses(expression : str) -> bool: # type hint
+    brackets = { ']': '[', '}': '{', ')': '(' }
     stack = list()
     for letter in expression:
-        if letter == '(':
-            stack.append("letter")
-        if letter == ')':
-            if len(stack) == 0:
-                return False # )1+2(), (1+2))
-            else:
-                stack.pop()
-    return len(stack) == 0 # (1+2), ((3*2)/2), ((3*2/2)
+        if letter in brackets.values():
+            stack.append(letter)
+        if letter in brackets.keys():
+            if not stack or stack.pop() != brackets[letter]:
+                return False
+    return not stack
 
+print(is_valid_parentheses("[({1+2)]}"))
+print(is_valid_parentheses("[({1+2})]"))
 print(is_valid_parentheses(")1+2()"))
 print(is_valid_parentheses("(1+2))"))
 print(is_valid_parentheses("(1+2)"))

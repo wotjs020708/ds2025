061367971079ed5bcca3d0b038e8f7f8b2dde647
JaeSunEo <wotjs020708@gmail.com>
2025-04-07 17:45:32 +0900

    v2.2 stack

--- week05.py
+++ week05.py
@@ -25,11 +25,18 @@ class Stack:
         popped_node.link = None
         return popped_node.data
 
+    def peek(self):
+        return self.top.data
+
 
 s1 = Stack()
-print(s1.pop())
+# print(s1.pop())
 s1.push("Data structure")
 s1.push("Database")
-print(s1.pop())
-print(s1.pop())
-print(s1.pop())
+# print(s1.pop())
+# print(s1.pop())
+s1.pop()
+print(s1.peek())
+
+# for i in range(2):
+#     print(s1.pop())

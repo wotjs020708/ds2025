a5ef4b018e105dd0e8b2f5452e9f3ad56994f883
JaeSunEo <wotjs020708@gmail.com>
2025-04-28 17:03:54 +0900

    3.2 tree

--- week08.py
+++ week08.py
@@ -4,7 +4,27 @@ class TreeNode:
         self.data = None
         self.right = None
 
+def pre_order(node):
+    if node is None:
+        return
+    print(node.data, end = "->")
+    pre_order(node.left)
+    pre_order(node.right)
 
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.data, end = "->")
+    in_order(node.right)
+
+
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node.data, end = "->")
 node1 = TreeNode()
 node1.data = 'hs'
 
@@ -28,5 +48,9 @@ node6 = TreeNode()
 node6.data = 'sm'
 node3.left = node6
 
-print(node6.data)
-print(node1.right.left.data)
+post_order(node1)
+print()
+pre_order(node1)
+print()
+in_order(node1)
+print()

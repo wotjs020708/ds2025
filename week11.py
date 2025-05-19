ff5583fda129de658c7361bc7d400d19a884ae2d
JaeSunEo <wotjs020708@gmail.com>
2025-05-19 17:02:51 +0900

    v4.4 graph

--- week11.py
+++ week11.py
@@ -3,6 +3,11 @@ class Graph:
         self.size = size
         self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
+    def print_graph(self, n):
+        for r in range(n):
+            for c in range(n):
+                print(self.graph[r][c], end=' ')
+            print()
 
 G1 = Graph(4)
 G2 = Graph(4)
@@ -14,22 +19,17 @@ G1.graph[1][0] = 1; G1.graph[1][2] = 1
 G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
 G1.graph[3][0] = 1; G1.graph[3][2] = 1
 
-print("G1 무방향 그래프")
-for r in range(G1.size):
-    for c in range(G1.size):
-        print(G1.graph[r][c], end=' ')
-    print()
 
+print("G1 무방향 그래프")
+
+G1.print_graph(G1.size)
 
 # 0 == A, 1 == B, 2 == C, 3 == D
 G2.graph[0][1] = 1; G2.graph[0][2] = 1
 G2.graph[3][0] = 1; G2.graph[3][2] = 1
 
-print("G3 방향 그래프")
-for r in range(G2.size):
-    for c in range(G2.size):
-        print(G2.graph[r][c], end=' ')
-    print()
+print("G2 방향 그래프")
+G2.print_graph(G2.size)
 
 # 0 == A, 1 == B, 2 == C, 3 == D
 G_self.graph[0][3] = 1
@@ -38,7 +38,4 @@ G_self.graph[2][1] = 1
 G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
 
 print("G_self 무방향 그래프")
-for r in range(G_self.size):
-    for c in range(G_self.size):
-        print(G_self.graph[r][c], end=' ')
-    print()
+G_self.print_graph(G_self.size)

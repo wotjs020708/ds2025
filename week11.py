914346714cbec8a97ee584fda09914864d36b27d
JaeSunEo <wotjs020708@gmail.com>
2025-05-19 18:03:18 +0900

    v4.5 graph

--- week11.py
+++ week11.py
@@ -1,41 +1,27 @@
-class Graph:
-    def __init__(self, size):
-        self.size = size
-        self.graph = [[0 for _ in range(size)] for _ in range(size)]
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
 
-    def print_graph(self, n):
-        for r in range(n):
-            for c in range(n):
-                print(self.graph[r][c], end=' ')
-            print()
+def dfs(g, i, visited):
+    visited[i] = True
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
-G1 = Graph(4)
-G2 = Graph(4)
-G_self = Graph(4)
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][0] = 1; G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
+def bfs(g, i, visited):
+    pass
 
 
-print("G1 무방향 그래프")
-
-G1.print_graph(G1.size)
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G2.graph[0][1] = 1; G2.graph[0][2] = 1
-G2.graph[3][0] = 1; G2.graph[3][2] = 1
-
-print("G2 방향 그래프")
-G2.print_graph(G2.size)
-
-# 0 == A, 1 == B, 2 == C, 3 == D
-G_self.graph[0][3] = 1
-G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
-G_self.graph[2][1] = 1
-G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
-
-print("G_self 무방향 그래프")
-G_self.print_graph(G_self.size)
+visited_dfs = [False for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+dfs(graph, 7, visited_dfs)
+print()
+bfs(graph, 4, visited_bfs)

2b8bf253b595d29480a3ef2cf157e305a1ca6b74
JaeSunEo <wotjs020708@gmail.com>
2025-05-26 17:05:02 +0900

    v4.8 deque

--- week12.py
+++ week12.py
@@ -1,11 +1,37 @@
 from collections import deque
 
-d = deque([91, 17, 33])
-d.append(-14)
-d.appendleft(100)
-d.append(99) # enqueue
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 1, 0]
+]
 
-for _ in range(len(d)):
-    print(d.popleft())  # dequeue
+def dfs(g, i, visited):
+    visited[i] = True
+    print(chr(ord('A')+i), end=' ')
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
+def bfs(g, i, visited):
+    queue = deque([i])
+    visited[i] = 1
+    # while len(queue) != 0:
+    while queue:
+        i = queue.popleft()
+        print(chr(ord('A')+i), end=' ')
+        for j in range(len(graph)):
+            if g[i][j] == 1 and not visited[j]:
+                queue.append(j)
+                visited[j] = 1
 
+visited_dfs = [False for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+dfs(graph, 7, visited_dfs)
+print()
+bfs(graph, 4, visited_bfs)

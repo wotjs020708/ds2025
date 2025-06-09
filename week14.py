ec416aa7b1c307ece105ea8dbe886070ded3d941
JaeSunEo <wotjs020708@gmail.com>
2025-06-09 17:35:32 +0900

    v5.5 boj 1929

--- week14.py
+++ week14.py
@@ -1,24 +1,27 @@
+import math
+
 def is_prime(n) -> bool:
-    if n <= 1:
-        return  False
-    else:
-        for i in range(2, n):
-            if n % i == 0:
-                return False
-    return True
-
-
+  if n <= 1:
+      return False
+  elif n == 2:
+      return True
+  elif n % 2 == 0:
+      return False
+  else:
+    for i in (3, int(math.sqrt(n)) + 1,2):
+        if n % i == 0:
+          return False
+  return True
 s, e = map(int, input().split())
 for i in range(s, e+1):
     if is_prime(i):
         print(i)
-print(s, e)
 
 
 # inputs = input().split()
-# t = []
+# t = []®
 #
-# for i in inputs:
+# for i in inputs:s
 #     t.append(int(i))
 #
 # print(t)
